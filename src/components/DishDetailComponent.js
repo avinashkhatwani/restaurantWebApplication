da1abb30df29cb4151f8e04b81d9301e1f7e5eb7
avinashkhatwani <46199402+avinashkhatwani@users.noreply.github.com>
2020-06-22 15:05:46 -0700

    Redux thunk (#6)
    * Redux Thunk, React-Redux-Forms
    
    * Redux thunk, server client - fetch and promise

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { baseUrl } from '../shared/baseUrl';
 import {
   Card,
   CardImg,
@@ -16,16 +17,17 @@ import {
   Label
 } from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { Loading } from './LoadingComponent';
 
 // import CommentForm from "./CommentFormComponent";
 
 function RenderDish(dish) {
   return (
       <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
+        <CardImg top src={baseUrl + dish.image} alt={dish.name} />
         <CardBody>
           <CardTitle>{dish.name}</CardTitle>
           <CardText>{dish.description}</CardText>
@@ -62,33 +64,54 @@ function RenderComments({ comments, addComment, dishId }) {
   }
 }
 
-const DishDetail = props => {
-  return (
-    <div className="container">
-      <div className="row">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link to="/menu">Menu</Link>
-          </BreadcrumbItem>
-          <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-        </Breadcrumb>
-        <div className="col-12">
-          <h3>{props.dish.name}</h3>
-          <hr />
+const DishDetail = (props) => {
+  if (props.isLoading) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <Loading />
+            </div>
+        </div>
+      );
+  }
+  else if (props.errMess) {
+      return(
+          <div className="container">
+              <div className="row">            
+                  <h4>{props.errMess}</h4>
+              </div>
+          </div>
+      );
+  }
+  else if(props.dish != null){
+    return (
+      <div className="container">
+        <div className="row">
+          <Breadcrumb>
+            <BreadcrumbItem>
+              <Link to="/menu">Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className="col-12">
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12 col-md-5 m-1">
+            <RenderDish {...props.dish} />
+          </div>
+          <div className="col-12 col-md-5 m-1">
+          <RenderComments comments={props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id}/>
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-12 col-md-5 m-1">
-          <RenderDish {...props.dish} />
-        </div>
-        <div className="col-12 col-md-5 m-1">
-        <RenderComments comments={props.comments}
-          addComment={props.addComment}
-          dishId={props.dish.id}/>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
+
 };
 
 // CommentForm Component and it's helper functions
@@ -127,7 +150,7 @@ class CommentForm extends Component {
           className="btn btn-light border border-dark"
           onClick={this.toggleModal}
         >
-          <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon>
+          {/* <FontAwesomeIcon icon={faPencilAlt}></FontAwesomeIcon> */}
           &nbsp; Submit comment
         </button>
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
   CardTitle, Breadcrumb, BreadcrumbItem  } from 'reactstrap';
-import {Link} from 'react-router-dom'
-
+import {Link} from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
   function RenderMenuItem({dish, onClick}) {
     // console.log(dish.id);
     return(
       <Card>
         <Link to={`/menu/${dish.id}`}>
-          <CardImg width='100%' src={dish.image} alt={dish.name} />
+          <CardImg width='100%' src={baseUrl + dish.image} alt={dish.name} />
           <CardImgOverlay>
             <CardTitle>{dish.name}</CardTitle>
           </CardImgOverlay>
@@ -19,14 +20,35 @@ import {Link} from 'react-router-dom'
 
   const Menu = (props) => {
 
-      const menu = props.dishes.map((dish) => {
+      const menu = props.dishes.dishes.map((dish) => {
+        
       return (
           <div key={dish.id} className="col-12 col-md-5">
             <RenderMenuItem dish={dish} />
           </div>
         );
       });
-
+      if (props.dishes.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.dishes.errMess) {
+        return(
+            <div className="container">
+                <div className="row"> 
+                    <div className="col-12">
+                        <h4>{props.dishes.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    else
       return (
         <div className="container">
             <div className="row">
@@ -43,7 +65,7 @@ import {Link} from 'react-router-dom'
                 {menu}
             </div>
         </div>
-    );
+      );
   }
 
 
